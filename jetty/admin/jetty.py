8c5ea59adafd6e15fd56f4b895e7098b82d28b00
Denis K <f1nal@cgaming.org>
2018-05-13 16:37:01 +0300

    Add initial related

--- jetty/admin/jetty.py
+++ jetty/admin/jetty.py
@@ -15,7 +15,25 @@ class JettyAdmin(object):
 
         return View
 
-    def register(self, Model, fields=None, actions=list(), ordering_field=None):
-        self.models.append(JettyAdminModelDescription(Model, fields, actions, ordering_field))
+    def register(self, Model, fields=None, actions=list(), ordering_field=None, hidden=False):
+        self.models.append(JettyAdminModelDescription(Model, fields, actions, ordering_field, hidden))
+
+    def register_related_models(self):
+        def model_key(x):
+            return '{}_{}'.format(
+                x['app_label'],
+                x['model']
+            )
+        registered = set(map(lambda x: model_key(x.get_model()), self.models))
+
+        for models_description in self.models:
+            for item in models_description.get_related_models():
+                key = model_key(item['model_info'])
+
+                if key in registered:
+                    continue
+
+                self.register(item['model'], hidden=True)
+                registered.add(key)
 
 jetty = JettyAdmin()
