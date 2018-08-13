8d885400c1ae62b6d9848e914c94c72699cf5191
Denis K <f1nal@cgaming.org>
2018-08-13 13:55:53 +0300

    Remove root view

--- jetty/urls.py
+++ jetty/urls.py
@@ -1,5 +1,3 @@
-from collections import OrderedDict
-
 from django.conf.urls import url
 from rest_framework.routers import DefaultRouter
 
@@ -14,13 +12,7 @@ from jetty.views.widget import WidgetViewSet
 
 def init_urls():
     class Router(DefaultRouter):
-        def get_api_root_view(self, api_urls=None):
-            api_root_dict = OrderedDict()
-            list_name = self.routes[0].name
-            for prefix, viewset, basename in self.registry:
-                api_root_dict[prefix] = list_name.format(basename=basename)
-            api_root_dict['model_descriptions'] = 'model-descriptions'
-            return self.APIRootView.as_view(api_root_dict=api_root_dict)
+        include_root_view = False
 
     router = Router()
 
