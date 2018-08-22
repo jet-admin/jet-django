7a5557f671907a7f3d3ec4daf03d8f1ab7134509
Denis K <f1nal@cgaming.org>
2018-08-22 20:07:05 +0300

    Fix auth

--- jetty/permissions.py
+++ jetty/permissions.py
@@ -1,11 +1,15 @@
+import uuid
+
 from rest_framework.permissions import BasePermission
 
 from jetty import settings
+from jetty.models.token import Token
 from jetty.utils.backend import project_auth
 
 
 class HasProjectPermissions(BasePermission):
-    token_prefixes = ['Token ', 'ProjectToken ']
+    token_prefix = 'Token '
+    project_token_prefix = 'ProjectToken '
 
     def has_permission(self, request, view):
         token = request.META.get('HTTP_AUTHORIZATION')
@@ -13,7 +17,18 @@ class HasProjectPermissions(BasePermission):
         if not token:
             return False
 
-        return any(map(lambda x: token[:len(x)] == x and project_auth(token[len(x):]), self.token_prefixes))
+        if token[:len(self.token_prefix)] == self.token_prefix:
+            token = token[len(self.token_prefix):]
+
+            return project_auth(token)
+        elif token[:len(self.project_token_prefix)] == self.project_token_prefix:
+            token = token[len(self.project_token_prefix):]
+            project_token = Token.objects.all().first()
+
+            return project_token.token == uuid.UUID(token)
+
+        else:
+            return False
 
 
 class ModifyNotInDemo(BasePermission):
