6a39b3160299c4c0b2d15bb46dd1209942702472
Denis K <f1nal@cgaming.org>
2018-09-23 18:31:16 +0300

    Add referrer

--- jet_django/views/register.py
+++ jet_django/views/register.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponseBadRequest
+from urllib.parse import quote
+
 from django.views import generic
 
 from jet_django import settings
@@ -6,11 +7,13 @@ from jet_django.utils.backend import register_token
 
 
 class RegisterView(generic.RedirectView):
-    def get(self, request, *args, **kwargs):
+    def get_redirect_url(self, *args, **kwargs):
         token, created = register_token()
 
         if not token:
-            return HttpResponseBadRequest
+            return
 
-        self.url = '{}/projects/register/{}'.format(settings.JET_BACKEND_WEB_BASE_URL, token.token)
-        return super().get(request, *args, **kwargs)
+        url = '{}/projects/register/{}'.format(settings.JET_BACKEND_WEB_BASE_URL, token.token)
+        query_string = 'referrer={}'.format(quote(self.request.build_absolute_uri().encode('utf8')))
+
+        return '%s?%s' % (url, query_string)
