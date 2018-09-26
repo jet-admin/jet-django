b951dba72d8a5eeb0720640a309a52d0df68334e
Denis K <f1nal@cgaming.org>
2018-09-26 15:40:42 +0300

    Fix file upload

--- jet_django/fields/raw_file.py
+++ jet_django/fields/raw_file.py
@@ -1,3 +1,6 @@
+import json
+from django.utils.translation import ugettext_lazy as _
+
 from jet_django.deps.rest_framework import serializers
 
 
@@ -6,7 +9,17 @@ class RawFileField(serializers.CharField):
     def __init__(self, *args, **kwargs):
         if 'validators' in kwargs:
             del kwargs['validators']
+
+        self.default_error_messages['invalid_format'] = _('Not a valid format.')
         super().__init__(*args, **kwargs)
+
+    def to_internal_value(self, data):
+        value = super().to_internal_value(data)
+        try:
+            obj = json.loads(value)
+            return obj['value']
+        except (ValueError, KeyError):
+            self.fail('invalid_format')
 
     def to_representation(self, value):
         return {
