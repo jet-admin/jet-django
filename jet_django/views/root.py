5ff0a29eb3649477cec59a53f3ed04a499344057
Denis K <f1nal@cgaming.org>
2019-01-24 11:27:03 +0300

    Add media url template

--- jet_django/views/root.py
+++ jet_django/views/root.py
@@ -1,3 +1,5 @@
+from django.core.files.storage import default_storage
+
 from jet_django.deps.rest_framework import views
 from jet_django.deps.rest_framework.response import Response
 
@@ -7,7 +9,9 @@ from jet_django.mixins.cors_api_view import CORSAPIViewMixin
 
 class RootView(CORSAPIViewMixin, views.APIView):
     def get(self, request, *args, **kwargs):
+        unique_placeholder = '__FILE_PATH___'
         return Response({
             'version': VERSION,
-            'type': 'jet_django'
+            'type': 'jet_django',
+            'media_url_template': request.build_absolute_uri(default_storage.url(unique_placeholder)).replace(unique_placeholder, '{}')
         })
