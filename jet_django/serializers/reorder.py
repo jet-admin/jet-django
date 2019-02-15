3ae411af6e7e3a9c2662ca094fb3c0987d339d33
Denis K <f1nal@cgaming.org>
2019-02-15 14:29:18 +0300

    Update reorder

--- jet_django/serializers/reorder.py
+++ jet_django/serializers/reorder.py
@@ -7,14 +7,23 @@ def reorder_serializer_factory(build_queryset):
     class ReorderSerializer(serializers.Serializer):
         ordering_field = serializers.CharField()
         forward = serializers.BooleanField()
-        segment_from = relations.PrimaryKeyRelatedField(queryset=build_queryset)
-        segment_to = relations.PrimaryKeyRelatedField(queryset=build_queryset)
+        segment_from = serializers.IntegerField()
+        segment_to = serializers.IntegerField()
         item = relations.PrimaryKeyRelatedField(queryset=build_queryset)
+        segment_by_ordering_field = serializers.BooleanField(default=False)
 
         def save(self):
             ordering_field = self.validated_data['ordering_field']
-            segment_from = getattr(self.validated_data['segment_from'], ordering_field)
-            segment_to = getattr(self.validated_data['segment_to'], ordering_field)
+
+            if self.validated_data.get('segment_by_ordering_field'):
+                segment_from = self.validated_data['segment_from']
+                segment_to = self.validated_data['segment_to']
+            else:
+                segment_from_instance = build_queryset.get(pk=self.validated_data['segment_from'])
+                segment_to_instance = build_queryset.get(pk=self.validated_data['segment_to'])
+
+                segment_from = getattr(segment_from_instance, ordering_field)
+                segment_to = getattr(segment_to_instance, ordering_field)
 
             with transaction.atomic():
                 if self.validated_data['forward']:
